7dd611c77243bcf113ae4e117831460868f0ca47
Minsecrus <2972853299@qq.com>
2024-02-24 14:37:15 +0800

    小修改

---
+++ 正文/小示例/unk_猜数游戏.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int secret = rand() % 100 + 1;
+    for (;;)
+    {
+        int guess;
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        if (guess == secret)
+        {
+            printf("You win!\n");
+            break;
+        }
+        else if (guess > secret)
+        {
+            printf("Too high!\n");
+        }
+        else
+        {
+            printf("Too low!\n");
+        }
+    }
+    return 0;
+}
