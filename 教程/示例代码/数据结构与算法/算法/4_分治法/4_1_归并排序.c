3b89619026773cec1013770b18c8e658ac120896
Minsecrus <2972853299@qq.com>
2024-08-02 20:04:54 +0800

    小修改

--- 教程/示例代码/数据结构与算法/算法/4_分治法/4_1_归并排序.c
+++ 教程/示例代码/数据结构与算法/算法/4_分治法/4_1_归并排序.c
@@ -7,10 +7,10 @@
 
 int N[10] = { 13, 5, 9, 6, 11, 2, 4, 7, 1, 8 };
 
-void merge(int A[], const int p, const int q, const int r)
+void merge(int A[], const int p, const int mid, const int r)
 {
-    const int n1 = q - p + 1;
-    const int n2 = r - q;
+    const int n1 = mid - p + 1;
+    const int n2 = r - mid;
     int* L = malloc((n1 + 1) * sizeof(int));
     int* R = malloc((n2 + 1) * sizeof(int));
     for (int i = 0; i < n1; i++) {
@@ -18,7 +18,7 @@ void merge(int A[], const int p, const int q, const int r)
     }
     L[n1] = INT_MAX;
     for (int i = 0; i < n2; i++) {
-        R[i] = A[q + i + 1];
+        R[i] = A[(mid + 1) + i];
     }
     R[n2] = INT_MAX;
     int i = 0, j = 0, k = p;
