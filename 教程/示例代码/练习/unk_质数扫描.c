892078b43b0a279a8e777180d97490ddbd3d7d29
Thid <123061703+Thid123@users.noreply.github.com>
2024-08-11 22:08:10 +0800

    Add files via upload

---
+++ 教程/示例代码/练习/unk_质数扫描.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    int t;
+    for (num = 101; num < 200; num++)
+    {
+        for (t = 2; t <= num; t++)
+        {
+            if (num == t)
+            {
+                printf("%d",num);
+            }
+            if (num % t == 0)
+            {
+                break;
+            }
+        }
+    }
+}
