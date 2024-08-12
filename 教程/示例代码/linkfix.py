7eafe685f9ff7cc5a2681821c9635c36f6dc77ad
LY-Xiang <3047563007@qq.com>
2024-08-12 11:34:05 +0800

    更新 linkfix.py

--- 教程/示例代码/linkfix.py
+++ 教程/示例代码/linkfix.py
@@ -5,4 +5,4 @@ for path, _, files in walk('.'):
     for file in files:
         if not file.endswith('.md'):
             with open(join(path, file) + '.md', 'w') as f:
-                f.write(f"[{file}](./{file} ':include')")
+                f.write(f"# {file}\n\n[{file}](./{file} ':include')\n")
